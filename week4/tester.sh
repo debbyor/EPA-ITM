0bf2cda66a455711cc8a0c823fdeb1a391bbbc63
epa <epa@localhost.localdomain>
2017-10-05 12:52:39 +0100

    Trial for ca

---
+++ week4/tester.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=30
+b=45
+
+if [ $a -eq $b ];
+then
+echo "Equal! On point"
+
+#elif [ $count -eq 30 ];
+#then 
+#echo "Count is equal to 30! Hurray !!!"
+
+else
+echo "Count is greater than 30, try again"
+
+fi

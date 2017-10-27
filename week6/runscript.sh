b871aa489d8b46f876f65b842cd25e515fe4e8ad
epa <epa@localhost.localdomain>
2017-10-27 14:57:48 +0100

    Preparing for cron

---
+++ week6/runscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo $HOME
+aws ec2 describe-instance-status --instance-id $1 > aws.txt
+
+cnt=`grep running aws.txt | wc -l`
+
+if [ $cnt -gt 0 ];
+
+then
+
+echo "The instance is running"
+
+else
+
+echo "No such instance"
+
+fi
+
